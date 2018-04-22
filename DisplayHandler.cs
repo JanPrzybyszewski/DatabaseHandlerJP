66f8acfd137bdcbbd791f0103051ea2fe1f838c7
Jan Przybyszewski <przybysz@student.agh.edu.pl>
2018-04-23 01:03:19 +0200

    adding files

---
+++ DisplayHandler.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Runtime.Remoting.Channels;
+
+namespace DataBaseHandler
+{
+    class DisplayHandler
+    {
+        // Handling database login
+
+        static public void InsertDBInfo(ref string server,ref string database,ref string ID,ref string pass)
+        {
+            Console.WriteLine("Welcome to the DatabaseHandler. First, insert Your db information to login: \n");
+            Console.WriteLine("Serwer name: ");
+            server = Console.ReadLine();
+            Console.WriteLine("\nDB name: ");
+            database = Console.ReadLine();
+            Console.WriteLine("\nUserID: ");
+            ID = Console.ReadLine();
+            Console.WriteLine("\nPassword: ");
+            pass = Console.ReadLine();
+            Console.Clear();
+        }
+        // Inserting table names into a list to display them.
+        static public void TableNamesIntoList(SqlConnection conn, List<string> tables)
+        {
+            tables.Clear();
+            int i = 1;
+            DataTable dt = conn.GetSchema("Tables");
+            foreach (DataRow row in dt.Rows)
+            {
+
+                string tablename = (string)row[2];
+                if (i != 1)
+                    tables.Add(tablename);
+                i++;
+            }
+        }
+        // Displaying Table names.
+        static public void WriteTableNames(List<string> tables)
+        {
+            Console.Clear();
+            Console.WriteLine("Tables in the current DB: \n");
+            foreach (var r in tables)
+            {
+                Console.WriteLine("\n" + r);
+            }
+            Console.WriteLine("\nPress ENTER to go back to Main Menu");
+            Console.ReadLine();
+        }
+
+        // Displaying the table.
+        public static void DisplaySpecificTable(string name, SqlConnection conn)
+        {
+
+            SqlCommand command = new SqlCommand("SELECT * FROM " + name, conn);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+              DisplayTable(reader);
+            }
+            Console.WriteLine("\nPress enter to go back to Main Menu");
+            Console.ReadLine();
+        }
+
+       
+       
+
+        // Columns displaying.
+
+        public static void DisplayTable(SqlDataReader reader)
+        {
+            var columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
+            int i = 0;
+            int length = reader.FieldCount;
+
+            while (i < length)
+            {
+                Console.Write(columns[i].ToString().PadRight(30));
+                i++;
+            }
+            Console.WriteLine();
+
+            while (reader.Read())
+            {
+                i = 0;
+                while (i < length)
+                {
+                    Console.Write(reader[i].ToString().PadRight(20));
+                    i++;
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Channels;
+
+namespace DataBaseHandler
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+            {
+                List<string> tables = new List<string>();
+                int actionChoice;
+                string server = "",database = "",userID = "",password = "",tableName;
+
+                DisplayHandler.InsertDBInfo(ref server,ref database,ref userID,ref password);
+
+           // Example: "Server=serwerlink;Database=JPDB1;User ID = serwerid;Password=**************;Trusted_Connection=False;Encrypt = True";
+
+            using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString =
+                        "Server=tcp:" + server + ";Database=" + database + ";User ID=" + userID + ";Password=" + password + ";Trusted_Connection=False;Encrypt = True";
+                    conn.Open();
+
+
+                    while(true)
+                    {
+                    Console.Clear();
+                    Console.WriteLine("\nWelcome to the DataBaseHandler. What do You want to do? \n\n \t 1 - See database tables' names \t 2 - See a specific database table \t 3 - EXIT\n Choose the number and type it in:");
+                    actionChoice = int.Parse(Console.ReadLine());
+
+
+                switch (actionChoice)
+                {
+                    case 1:
+                    {
+                        DisplayHandler.TableNamesIntoList(conn,tables);
+                        DisplayHandler.WriteTableNames(tables);
+                        break;
+                    }
+                    case 2:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\nInsert table name: ");
+                        tableName = Console.ReadLine();
+                        DisplayHandler.DisplaySpecificTable(tableName,conn);
+                        break;
+                    }
+                    case 3:
+                    {
+                        return;
+                    }
+                    default:
+                    {
+                        Console.WriteLine("Insert a proper table\n");
+                        break;
+                    }
+                }
+                    };
+
+                }
+            }
+        }
+    }
+
